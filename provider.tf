f564d3cde2eadb3bcc0729d7bf703d8b913d57dc
Mohammad Abbas <abbas.devops@gmail.com>
2020-04-15 16:39:02 +0530

    modified provide.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region = "us-east-2"
-  access_key = "AKIATCXS62HS6OC7577P"
-  secret_key = "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"
+  region                  = "us-east-2"
+  shared_credentials_file = "/Users/DevOps/.aws/credentials"
 }
